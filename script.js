b3fbcc3a37047e7a067da1790f4e3d2f178c167b
Leandro Maselli <leoroan@gmail.com>
2024-01-16 13:58:16 -0300

    updated

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-function save() {
-  // Get input values
+document.getElementById('saveButton').setAttribute('data-bs-dismiss', 'modal');
+
+function save() {  // Get input values
   var tituloValue = document.getElementById("titulo").value;
   var subtituloValue = document.getElementById("subtitulo").value;
   var descripcionValue = document.getElementById("descripcion").value;
-
   // Set values to specified element IDs
   document.getElementById("mainTitulo").innerText = tituloValue;
   document.getElementById("mainSubtitulo").innerText = subtituloValue;
   document.getElementById("mainDescripcion").innerText = descripcionValue;
+  
+}
+
+function reset() {
+  document.getElementById("titulo").value = "";
+  document.getElementById("subtitulo").value = "";
+  document.getElementById("descripcion").value = "";  
+  alert("resetewd")
 }
 
 function copyToClipboard(elementId) {
@@ -19,4 +27,8 @@ function copyToClipboard(elementId) {
    document.execCommand('copy');
    document.body.removeChild(textarea);
    alert('HTML copiado al portapapeles!');
+}
+
+function downloadHtml(elementId) {
+  alert("⛔ under development ⛔")  
 }
