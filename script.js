53cf3f4783fac568f770293be3128701acf5d4a5
Leandro Maselli <leoroan@gmail.com>
2024-01-16 13:15:02 -0300

    updated with borders

--- script.js
+++ script.js
@@ -11,25 +11,12 @@ function save() {
 }
 
 function copyToClipboard(elementId) {
-   // Get the element with the specified ID
    var element = document.getElementById(elementId);
-
-   // Create a textarea element to hold the inner HTML
    var textarea = document.createElement('textarea');
    textarea.value = element.innerHTML;
-
-   // Append the textarea to the body
    document.body.appendChild(textarea);
-
-   // Select the textarea content
    textarea.select();
-
-   // Execute the "copy" command
    document.execCommand('copy');
-
-   // Remove the temporary textarea
    document.body.removeChild(textarea);
-
-   // Provide user feedback (optional)
-   alert('Inner HTML copied to clipboard!');
+   alert('HTML copiado al portapapeles!');
 }
